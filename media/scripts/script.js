da5b187bab6fe4653f2cc7595106f93dde1fbe54
Andreas Kwiatkowski <derkwiat@yahoo.de>
2012-06-24 16:18:06 +0200

    Page for Play! Authenticate

---
+++ media/scripts/script.js
@@ -0,0 +1,12 @@
+$(document).ready(function(){
+$('#slider').bjqs({
+'width' : 800,
+'height' : 545,
+'showMarkers' : false,
+'showControls' : false,
+'centerMarkers' : false,
+'automatic' : true,
+'animation' : 'fade',
+'hoverPause' : true
+});
+});

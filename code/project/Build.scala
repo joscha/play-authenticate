49c5aa2d17450b4ed797f2dbc1ded6965718ee47
Joscha Feth <joscha@feth.com>
2012-06-25 02:41:11 +0200

    bump version

--- code/project/Build.scala
+++ code/project/Build.scala
@@ -5,7 +5,7 @@ import PlayProject._
 object ApplicationBuild extends Build {
 
     val appName         = "play-authenticate"
-    val appVersion      = "0.1.1-SNAPSHOT"
+    val appVersion      = "0.1.2-SNAPSHOT"
 
     val appDependencies = Seq(
       "org.apache.httpcomponents" % "httpclient" % "4.2",

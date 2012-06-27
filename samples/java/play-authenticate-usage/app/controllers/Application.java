707f4780b294c99a0d66172bc9dd48c83b7c4196
Enalmada <enalmada@gmail.com>
2012-06-26 22:30:03 -0700

    Update master

--- samples/java/play-authenticate-usage/app/controllers/Application.java
+++ samples/java/play-authenticate-usage/app/controllers/Application.java
@@ -38,7 +38,8 @@ public class Application extends Controller {
 		return ok(restricted.render(localUser));
 	}
 	
-	@Restrict(Application.USER_ROLE
+	// Fixing missing right paren
+	@Restrict(Application.USER_ROLE)
 	public static Result profile() {
 		final User localUser = getLocalUser(session());
 		return ok(profile.render(localUser));

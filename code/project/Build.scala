f786642a4ee5f401090ae8af79e90c0714605647
Joscha Feth <joscha@feth.com>
2012-07-08 22:14:39 +0200

    make dependencies library dependencies - not exposed to dependent projects

--- code/project/Build.scala
+++ code/project/Build.scala
@@ -7,15 +7,16 @@ object ApplicationBuild extends Build {
     val appName         = "play-authenticate"
     val appVersion      = "0.1.7-SNAPSHOT"
 
-    val appDependencies = Seq(
-      "org.apache.httpcomponents" % "httpclient" % "4.2",
-      "com.feth" %% "play-easymail" % "0.1-SNAPSHOT",
-      "org.mindrot" % "jbcrypt" % "0.3m"
-    )
-
-    val main = PlayProject(appName, appVersion, appDependencies, mainLang = JAVA).settings(
+    val main = PlayProject(appName, appVersion, mainLang = JAVA).settings(
       organization := "com.feth",
       resolvers += "Apache" at "http://repo1.maven.org/maven2/",
-      resolvers += "jBCrypt Repository" at "http://repo1.maven.org/maven2/org/"
+      resolvers += "jBCrypt Repository" at "http://repo1.maven.org/maven2/org/",
+
+      resolvers += Resolver.url("play-easymail (release)", url("http://joscha.github.com/play-easymail/repo/releases/"))(Resolver.ivyStylePatterns),
+      resolvers += Resolver.url("play-easymail (snapshot)", url("http://joscha.github.com/play-easymail/repo/snapshots/"))(Resolver.ivyStylePatterns),
+
+      libraryDependencies += "org.apache.httpcomponents" % "httpclient" % "4.2",
+      libraryDependencies += "com.feth" %% "play-easymail" % "0.1-SNAPSHOT",
+      libraryDependencies += "org.mindrot" % "jbcrypt" % "0.3m"
     )
 }

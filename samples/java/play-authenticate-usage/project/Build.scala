c154d810e0c733e7b8ebdb639a415b310b08d3ab
Joscha Feth <joscha@feth.com>
2012-07-12 23:39:33 +0200

    Update instructions for local dev

--- samples/java/play-authenticate-usage/project/Build.scala
+++ samples/java/play-authenticate-usage/project/Build.scala
@@ -8,15 +8,25 @@ object ApplicationBuild extends Build {
     val appVersion      = "1.0-SNAPSHOT"
 
     val appDependencies = Seq(
-      "be.objectify" 	%% 	"deadbolt-2"		% "1.1.3-SNAPSHOT",
-      "com.feth"		%% 	"play-authenticate"	% "0.1.8-SNAPSHOT",
-      "postgresql"		% 	"postgresql"		% "9.1-901.jdbc4"
+      "be.objectify"  %%  "deadbolt-2"        % "1.1.3-SNAPSHOT",
+      "com.feth"      %%  "play-authenticate" % "0.1.8-SNAPSHOT",
+      "postgresql"    %   "postgresql"        % "9.1-901.jdbc4"
     )
     
-//    Uncomment this for local development of the Play Authenticate core:
-//    val playAuthenticate = PlayProject(
-//     "play-authenticate", "1.0-SNAPSHOT", Seq("org.mindrot" % "jbcrypt" % "0.3m","com.typesafe" %% "play-plugins-mailer" % "2.0.2"), mainLang = JAVA, path = file("modules/play-authenticate")
-//    )
+//  Uncomment this for local development of the Play Authenticate core:
+/*
+    val playAuthenticate = PlayProject(
+      "play-authenticate", "1.0-SNAPSHOT", mainLang = JAVA, path = file("modules/play-authenticate")
+    ).settings(
+      libraryDependencies += "org.apache.httpcomponents" % "httpclient" % "4.2",
+      libraryDependencies += "com.feth" %% "play-easymail" % "0.1-SNAPSHOT",
+      libraryDependencies += "org.mindrot" % "jbcrypt" % "0.3m",
+      libraryDependencies += "commons-lang" % "commons-lang" % "2.6",
+
+      resolvers += Resolver.url("play-easymail (release)", url("http://joscha.github.com/play-easymail/repo/releases/"))(Resolver.ivyStylePatterns),
+      resolvers += Resolver.url("play-easymail (snapshot)", url("http://joscha.github.com/play-easymail/repo/snapshots/"))(Resolver.ivyStylePatterns)
+    )
+*/
 
     val main = PlayProject(appName, appVersion, appDependencies, mainLang = JAVA).settings(
       resolvers += Resolver.url("Objectify Play Repository", url("http://schaloner.github.com/releases/"))(Resolver.ivyStylePatterns),
@@ -28,7 +38,7 @@ object ApplicationBuild extends Build {
       resolvers += Resolver.url("play-authenticate (release)", url("http://joscha.github.com/play-authenticate/repo/releases/"))(Resolver.ivyStylePatterns),
       resolvers += Resolver.url("play-authenticate (snapshot)", url("http://joscha.github.com/play-authenticate/repo/snapshots/"))(Resolver.ivyStylePatterns)
     )
-    // Uncomment this for local development of the Play Authenticate core:
-    //.dependsOn(playAuthenticate)
+//  Uncomment this for local development of the Play Authenticate core:
+//    .dependsOn(playAuthenticate).aggregate(playAuthenticate)
 
 }

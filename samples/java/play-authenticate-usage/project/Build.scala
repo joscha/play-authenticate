7563ea9c33f8c72b109e0a31951c9f04b85efd1f
Marcus Biesioroff <marcus@biesioroff.com>
2012-08-02 19:50:54 +0200

    Refactored sample to make it more independent Added multilang better support, like manual language changing or label falldown (finding English label in case of missing label in choosen language) Added Polish translation Added description with checklist on how to insert play-authenticate into own app

--- samples/java/play-authenticate-usage/project/Build.scala
+++ samples/java/play-authenticate-usage/project/Build.scala
@@ -8,8 +8,10 @@ object ApplicationBuild extends Build {
     val appVersion      = "1.0-SNAPSHOT"
 
     val appDependencies = Seq(
+    // Play Authenticate START
       "be.objectify"  %%  "deadbolt-2"        % "1.1.3-SNAPSHOT",
       "com.feth"      %%  "play-authenticate" % "0.2.0-SNAPSHOT",
+      // Play Authenticate END
       "postgresql"    %   "postgresql"        % "9.1-901.jdbc4"
     )
     
@@ -29,6 +31,7 @@ object ApplicationBuild extends Build {
 */
 
     val main = PlayProject(appName, appVersion, appDependencies, mainLang = JAVA).settings(
+      // Play Authenticate START
       resolvers += Resolver.url("Objectify Play Repository", url("http://schaloner.github.com/releases/"))(Resolver.ivyStylePatterns),
       resolvers += Resolver.url("Objectify Play Repository", url("http://schaloner.github.com/snapshots/"))(Resolver.ivyStylePatterns),
 
@@ -37,6 +40,7 @@ object ApplicationBuild extends Build {
 
       resolvers += Resolver.url("play-authenticate (release)", url("http://joscha.github.com/play-authenticate/repo/releases/"))(Resolver.ivyStylePatterns),
       resolvers += Resolver.url("play-authenticate (snapshot)", url("http://joscha.github.com/play-authenticate/repo/snapshots/"))(Resolver.ivyStylePatterns)
+      // Play Authenticate END
     )
 //  Uncomment this for local development of the Play Authenticate core:
 //    .dependsOn(playAuthenticate).aggregate(playAuthenticate)

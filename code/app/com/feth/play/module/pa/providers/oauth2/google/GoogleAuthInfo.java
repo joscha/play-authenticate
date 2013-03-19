84a3968ac7dc91163c37dfe0229a622b8aced140
Joe Cerra <jcerra@gmail.com>
2013-03-19 08:51:07 -0400

    Added refresh_token and expires_in in GoogleAuthInfo

--- code/app/com/feth/play/module/pa/providers/oauth2/google/GoogleAuthInfo.java
+++ code/app/com/feth/play/module/pa/providers/oauth2/google/GoogleAuthInfo.java
@@ -1,11 +1,10 @@
 package com.feth.play.module.pa.providers.oauth2.google;
 
-import java.util.Date;
-
-import org.codehaus.jackson.JsonNode;
-
 import com.feth.play.module.pa.providers.oauth2.OAuth2AuthInfo;
 import com.feth.play.module.pa.providers.oauth2.OAuth2AuthProvider.Constants;
+import org.codehaus.jackson.JsonNode;
+
+import java.util.Date;
 
 public class GoogleAuthInfo extends OAuth2AuthInfo {
 
@@ -17,12 +16,16 @@ public class GoogleAuthInfo extends OAuth2AuthInfo {
 	private static final String ID_TOKEN = "id_token";
 	private String bearer;
 	private String idToken;
+    private String refreshToken;
+    private long expiresIn;
 
 	public GoogleAuthInfo(final JsonNode node) {
 		super(node.get(Constants.ACCESS_TOKEN).asText(), new Date().getTime()
 				+ node.get(Constants.EXPIRES_IN).asLong() * 1000);
 		bearer = node.get(Constants.TOKEN_TYPE).asText();
 		idToken = node.get(ID_TOKEN).asText();
+        refreshToken = node.get(Constants.REFRESH_TOKEN).asText();
+        expiresIn = node.get(Constants.EXPIRES_IN).asLong();
 	}
 
 	public String getBearer() {
@@ -32,4 +35,12 @@ public class GoogleAuthInfo extends OAuth2AuthInfo {
 	public String getIdToken() {
 		return idToken;
 	}
+
+    public String getRefreshToken() {
+        return refreshToken;
+    }
+
+    public long getExpiresIn() {
+        return expiresIn;
+    }
 }

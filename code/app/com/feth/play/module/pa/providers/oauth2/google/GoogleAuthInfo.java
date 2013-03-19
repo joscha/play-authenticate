5b5fab42cebabf18076ced674f734636c269791e
Joe Cerra <jcerra@gmail.com>
2013-03-19 09:32:54 -0400

    Revert "Added refresh_token and expires_in in GoogleAuthInfo"
    This reverts commit 84a3968ac7dc91163c37dfe0229a622b8aced140.

--- code/app/com/feth/play/module/pa/providers/oauth2/google/GoogleAuthInfo.java
+++ code/app/com/feth/play/module/pa/providers/oauth2/google/GoogleAuthInfo.java
@@ -1,10 +1,11 @@
 package com.feth.play.module.pa.providers.oauth2.google;
 
-import com.feth.play.module.pa.providers.oauth2.OAuth2AuthInfo;
-import com.feth.play.module.pa.providers.oauth2.OAuth2AuthProvider.Constants;
+import java.util.Date;
+
 import org.codehaus.jackson.JsonNode;
 
-import java.util.Date;
+import com.feth.play.module.pa.providers.oauth2.OAuth2AuthInfo;
+import com.feth.play.module.pa.providers.oauth2.OAuth2AuthProvider.Constants;
 
 public class GoogleAuthInfo extends OAuth2AuthInfo {
 
@@ -16,16 +17,12 @@ public class GoogleAuthInfo extends OAuth2AuthInfo {
 	private static final String ID_TOKEN = "id_token";
 	private String bearer;
 	private String idToken;
-    private String refreshToken;
-    private long expiresIn;
 
 	public GoogleAuthInfo(final JsonNode node) {
 		super(node.get(Constants.ACCESS_TOKEN).asText(), new Date().getTime()
 				+ node.get(Constants.EXPIRES_IN).asLong() * 1000);
 		bearer = node.get(Constants.TOKEN_TYPE).asText();
 		idToken = node.get(ID_TOKEN).asText();
-        refreshToken = node.get(Constants.REFRESH_TOKEN).asText();
-        expiresIn = node.get(Constants.EXPIRES_IN).asLong();
 	}
 
 	public String getBearer() {
@@ -35,12 +32,4 @@ public class GoogleAuthInfo extends OAuth2AuthInfo {
 	public String getIdToken() {
 		return idToken;
 	}
-
-    public String getRefreshToken() {
-        return refreshToken;
-    }
-
-    public long getExpiresIn() {
-        return expiresIn;
-    }
 }

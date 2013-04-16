97504cfc30f5572a995d761a33ed9fb8438d811f
Tim Dettrick <t.dettrick@uq.edu.au>
2013-04-16 11:52:21 +1000

    Adding test application for automated testing.
    Scala compatibility is limited in play-authenticate currently, but not
    entirely absent. Automated testing should improve this situation.
    
    This test application provides a very basic automated testing
    environment, which implements a UsernamePasswordAuthProvider and
    associated support classes in Java, and tests controller protection for
    Java & Scala.
    
    The main aim is to provide very basic test coverage of core
    play-authenticate functionality.
    
    Tests of the Scala controller are written in Scala too.

---
+++ test-app/test/ScalaControllerSpec.scala
@@ -0,0 +1,96 @@
+package test
+
+import org.specs2.mutable._
+
+import play.api.Logger
+import play.api.Play
+import play.api.mvc.AnyContentAsEmpty
+import play.api.mvc.AnyContentAsEmpty
+import play.api.mvc.Call
+import play.api.mvc.Result
+import play.api.mvc.Session
+import play.api.test._
+import play.api.test.Helpers._
+import providers.TestUsernamePasswordAuthProvider
+
+/**
+ * Add your spec here.
+ * You can mock out a whole application including requests, plugins etc.
+ * For more information, consult the wiki.
+ */
+class ScalaControllerSpec extends Specification {
+
+  "ScalaController" should {
+
+    "send 303 for index page without login" in {
+      running(FakeApplication()) {
+        val result = await(
+          controllers.ScalaController.index()(FakeRequest()).run)
+        status(result) must_== SEE_OTHER;
+        redirectLocation(result) must beSome like { case Some(s: String) =>
+          s must_== controllers.routes.Application.login.url
+        }
+      }
+    }
+
+    "send 200 for index page with login" in {
+      running(FakeApplication()) {
+        val someSession = signupAndLogin()
+        val result = await(
+          controllers.ScalaController.index()(
+              FakeRequest().withSession(someSession.get.data.toSeq: _*)).run)
+        status(result) must equalTo(OK)
+      }
+    }
+
+    def signupAndLogin(): Option[Session] = {
+      val email = "user@example.com"
+      val password = "PaSSW0rd"
+      def fakeRequestCall(call: Call): FakeRequest[AnyContentAsEmpty.type] = {
+        FakeRequest(call.method, call.url)
+      }
+      def signup(email: String, password: String) = {
+        val someResult = route(fakeRequestCall(
+          controllers.routes.Application.doSignup())
+          .withFormUrlEncodedBody("email" -> email, "password" -> password))
+        someResult foreach { status(_) must_== SEE_OTHER }
+        upAuthProvider.getVerificationToken(email)
+      }
+      def validate(token: String) {
+        // Validate the token
+        token must not beNull;
+        Logger.debug(s"Verifying token: $token")
+        val someResult = route(fakeRequestCall(
+          controllers.routes.Application.verify(token)))
+        someResult foreach { result =>
+          status(result) must_== SEE_OTHER
+          upAuthProvider.getVerificationToken(email) must beNull
+          // We should actually be logged in here, but let's ignore that
+          // as we want to test login too.
+          redirectLocation(result) must beSome("/")
+        }
+      }
+      def login(username: String, password: String) = {
+        // Log the user in
+        val someResult = route(fakeRequestCall(
+          controllers.routes.Application.doLogin())
+          .withFormUrlEncodedBody("email" -> email, "password" -> password))
+        someResult map { result =>
+          status(result) must_== SEE_OTHER
+          redirectLocation(result) must beSome like { case Some(s: String) =>
+            s must_== "/"
+          }
+          session(result)
+        }
+      }
+      val token = signup(email, password)
+      validate(token)
+      login(email, password)
+    }
+
+    def upAuthProvider: TestUsernamePasswordAuthProvider = {
+      Play.current.plugin(classOf[TestUsernamePasswordAuthProvider]).get
+    }
+
+  }
+}

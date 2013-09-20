1ec5d5950ad93a0081e67d8c0ebe7482949198d4
Andrea Guarini <guarini.andrea@hotmail.it>
2013-09-20 13:08:01 +0200

    Renamed 'token' variable to 'requestToken'

--- code/app/com/feth/play/module/pa/providers/oauth2/pocket/PocketAuthInfo.java
+++ code/app/com/feth/play/module/pa/providers/oauth2/pocket/PocketAuthInfo.java
@@ -5,19 +5,19 @@ import com.feth.play.module.pa.providers.oauth2.OAuth2AuthInfo;
 public class PocketAuthInfo extends OAuth2AuthInfo {
 
 	private static final long serialVersionUID = 1L;
-	private String token;
+	private String requestToken;
 
-	public PocketAuthInfo(final String accessToken, final String token) {
+	public PocketAuthInfo(final String accessToken, final String requestToken) {
 		super(accessToken);
-		this.token = token;
+		this.requestToken = requestToken;
 	}
 
-	public String getToken() {
-		return token;
+	public String getRequestToken() {
+		return requestToken;
 	}
 
-	public void setToken(String token) {
-		this.token = token;
+	public void setRequestToken(String requestToken) {
+		this.requestToken = requestToken;
 	}
 
 }

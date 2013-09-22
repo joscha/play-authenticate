6f0d724bea0fac4090703c897678a732e6b934c6
Joscha Feth <joscha@feth.com>
2013-09-22 13:33:20 +0200

    wip

--- code/project/Build.scala
+++ code/project/Build.scala
@@ -5,7 +5,7 @@ import play.Project._
 object ApplicationBuild extends Build {
 
     val appName         = "play-authenticate"
-    val appVersion      = "0.3.3-SNAPSHOT"
+    val appVersion      = "0.3.4-SNAPSHOT"
 
     val appDependencies = Seq(
         javaCore

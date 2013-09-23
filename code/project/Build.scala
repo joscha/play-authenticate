74a511ab7241bbfb3e4de4c3cac00d40308b8d68
Evgeniy Goryuchkin <goryuchkin@gmail.com>
2013-09-23 09:58:22 +0300

    change repos back to joscha.github.com

--- code/project/Build.scala
+++ code/project/Build.scala
@@ -17,8 +17,8 @@ object ApplicationBuild extends Build {
       resolvers += "Apache" at "http://repo1.maven.org/maven2/",
       resolvers += "jBCrypt Repository" at "http://repo1.maven.org/maven2/org/",
 
-      resolvers += Resolver.url("play-easymail (release)", url("http://repo.laf.su/releases/"))(Resolver.ivyStylePatterns),
-      resolvers += Resolver.url("play-easymail (snapshot)", url("http://repo.laf.su/snapshots/"))(Resolver.ivyStylePatterns),
+      resolvers += Resolver.url("play-easymail (release)", url("http://joscha.github.com/play-easymail/repo/releases/"))(Resolver.ivyStylePatterns),
+      resolvers += Resolver.url("play-easymail (snapshot)", url("http://joscha.github.com/play-easymail/repo/snapshots/"))(Resolver.ivyStylePatterns),
 
       libraryDependencies += "org.apache.httpcomponents" % "httpclient" % "4.2.5",
       libraryDependencies += "com.feth" %% "play-easymail" % "0.3-SNAPSHOT",

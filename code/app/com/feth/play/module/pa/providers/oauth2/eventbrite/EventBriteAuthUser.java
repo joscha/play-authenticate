e9e1007ff8a8e7f7d6505c62bd1942d30e471c3e
Keyur <keyur.raval18@gmail.com>
2014-07-05 11:40:26 +0530

    EventBrite Auth Provider

---
+++ code/app/com/feth/play/module/pa/providers/oauth2/eventbrite/EventBriteAuthUser.java
@@ -0,0 +1,118 @@
+package com.feth.play.module.pa.providers.oauth2.eventbrite;
+
+import java.util.Locale;
+
+import play.Logger;
+
+import com.fasterxml.jackson.databind.JsonNode;
+import com.feth.play.module.pa.providers.oauth2.BasicOAuth2AuthUser;
+import com.feth.play.module.pa.user.ExtendedIdentity;
+import com.feth.play.module.pa.user.LocaleIdentity;
+import com.feth.play.module.pa.user.PicturedIdentity;
+import com.feth.play.module.pa.user.ProfiledIdentity;
+
+public class EventBriteAuthUser extends BasicOAuth2AuthUser implements
+		ExtendedIdentity, PicturedIdentity, ProfiledIdentity, LocaleIdentity {
+
+	private static final long serialVersionUID = 1L;
+
+	private static abstract class Constants {
+		public static final String ID = "id"; 
+		public static final String NAME = "name"; 
+		public static final String FIRST_NAME = "first_name";
+		public static final String LAST_NAME = "last_name"; 
+		public static final String EMAIL = "email";
+		public static final String EMAILS = "emails";
+		public static final String PRIMARY = "primary" ;
+	}
+
+	private String name;
+	private String firstName;
+	private String lastName;
+	private String email;
+	private boolean verified = false;
+
+	public EventBriteAuthUser(final JsonNode node,
+			final EventBriteAuthInfo info, final String state) {
+		super(node.get(Constants.ID).asText(), info, state);
+
+		if (node.has(Constants.FIRST_NAME)) {
+			this.firstName = node.get(Constants.FIRST_NAME).asText();
+		}
+		if (node.has(Constants.LAST_NAME)) {
+			this.lastName = node.get(Constants.LAST_NAME).asText();
+		}
+		if (node.has(Constants.NAME)) {
+			this.name = node.get(Constants.NAME).asText();
+		}
+		Logger.debug("Emails : " + node.get(Constants.EMAILS));
+		if (node.has(Constants.EMAILS)) {
+			JsonNode settingsNode = node.get(Constants.EMAILS);
+			Logger.debug("Settings Node : " + settingsNode);
+			Logger.debug("Has email node : " + settingsNode.has(Constants.EMAIL));
+			for(JsonNode jsonNode : settingsNode)
+			{
+				Logger.debug("Verified " + jsonNode.get(Constants.PRIMARY).asBoolean() );
+				if(jsonNode.get(Constants.PRIMARY).asBoolean())
+				{
+					Logger.debug(jsonNode.get(Constants.EMAIL).asText());
+					this.email = jsonNode.get(Constants.EMAIL).asText();
+					break;
+					
+				}
+			}
+		}
+
+	}
+
+	@Override
+	public String getProvider() {
+		return EventBriteAuthProvider.PROVIDER_KEY;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public String getFirstName() {
+		return firstName;
+	}
+
+	public String getLastName() {
+		return lastName;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public boolean isVerified() {
+		return verified;
+	}
+
+	@Override
+	public Locale getLocale() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public String getProfileLink() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public String getPicture() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public String getGender() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+
+}

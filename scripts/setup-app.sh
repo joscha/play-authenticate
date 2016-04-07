4b3fe4965713f218df5950eedb555b89f53f0519
Joscha Feth <joscha@feth.com>
2016-04-07 22:23:22 +1000

    fix: setup script

--- scripts/setup-app.sh
+++ scripts/setup-app.sh
@@ -8,4 +8,13 @@ curl -X POST https://api.heroku.com/apps \
 -H "Accept: application/vnd.heroku+json; version=3" \
 -H "Authorization: Bearer $HEROKU_API_KEY" \
 -H "Content-Type: application/json" \
--d "{\"name\":\"play-authenticate\", \"addons\": [\"heroku-postgresql:dev\"] }"
+-d '{
+    "name": "play-authenticate"
+}'
+
+curl -n -X POST https://api.heroku.com/apps/play-authenticate/addons \
+-H "Content-Type: application/json" \
+-H "Accept: application/vnd.heroku+json; version=3" \
+-d '{
+    "plan": "heroku-postgresql:dev"
+}'

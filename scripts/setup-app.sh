b7162c86f3163da69542d740880ad4b0ea573cf7
Joscha Feth <joscha@feth.com>
2016-04-08 09:54:47 +1000

    chore: attach DB addon

--- scripts/setup-app.sh
+++ scripts/setup-app.sh
@@ -1,9 +1,12 @@
 #!/bin/sh
+set -e
 
+echo "Delete app"
 curl -X DELETE https://api.heroku.com/apps/play-authenticate \
 -H "Accept: application/vnd.heroku+json; version=3" \
 -H "Authorization: Bearer $HEROKU_API_KEY"
 
+echo "Create app"
 curl -X POST https://api.heroku.com/apps \
 -H "Accept: application/vnd.heroku+json; version=3" \
 -H "Authorization: Bearer $HEROKU_API_KEY" \
@@ -12,10 +15,14 @@ curl -X POST https://api.heroku.com/apps \
     "name": "play-authenticate"
 }'
 
-curl -n -X POST https://api.heroku.com/apps/play-authenticate/addons \
--H "Accept: application/vnd.heroku+json; version=3" \
--H "Authorization: Bearer $HEROKU_API_KEY" \
+echo "Attach postgres addon"
+curl -n -X POST https://api.heroku.com/addon-attachments \
 -H "Content-Type: application/json" \
+-H "Authorization: Bearer $HEROKU_API_KEY" \
+-H "Accept: application/vnd.heroku+json; version=3" \
 -d '{
-    "plan": "heroku-postgresql:dev"
+  "addon": "heroku-postgresql:dev",
+  "app": "play-authenticate",
+  "force": false,
+  "name": "DATABASE"
 }'

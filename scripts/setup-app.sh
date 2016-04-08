3b9cad6f2da1ecfa6e8af3de47426f3481f0aac5
Joscha Feth <joscha@feth.com>
2016-04-08 11:28:42 +1000

    chore: use toolbelt instead of gem

--- scripts/setup-app.sh
+++ scripts/setup-app.sh
@@ -2,7 +2,6 @@
 set -e
 set -x
 
-gem install heroku -V
 heroku apps:destroy --app play-authenticate --confirm play-authenticate
 heroku apps:create play-authenticate
 heroku addons:create heroku-postgresql:hobby-dev --app play-authenticate
